9985899bd1c438f1f3bad0ec2a8d9d16ce5286d1
Matt McManus <matt@mcmanus.io>
2013-12-19 10:39:01 -0500

    Everything works on RHEL now...i think

--- bootstrap-rhel.sh
+++ bootstrap-rhel.sh
@@ -1,48 +1,46 @@
 #!/bin/bash
 
+graylog2_server="graylog2-server-0.20.0-preview.8"
+graylog2_ui="graylog2-web-interface-0.20.0-preview.8"
+
 # Install Mongo
 [[ ! `which mongod` ]] &&
 (
   echo " --- Installing MongoDB"
-  sudo apt-key add /vagrant/10gen-gpg-key.asc
-  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-  sudo apt-get update -qq
-  sudo apt-get install mongodb-10gen -y -qq
+  cp /vagrant/config/mongodb.repo /etc/yum.repos.d/
+  sudo yum install mongo-10gen mongo-10gen-server -y -q
 )
 
 # Installing other dependancies
 echo " --- Installing JDK"
-sudo apt-get install openjdk-7-jre-headless -y -qq
+sudo yum install java-1.7.0-openjdk.x86_64 -y -q
+
 
 # from https://gist.github.com/wingdspur/2026107
 [[ ! -d /etc/elasticsearch ]] &&
 (
   echo " --- Installing ElasticSearch"
-  sudo dpkg -i /vagrant/elasticsearch-0.90.7.deb
-  sudo cp /vagrant/elasticsearch.yml /etc/elasticsearch/
-  sudo service elasticsearch start
+  sudo yum localinstall https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-0.90.7.noarch.rpm -y -q
+  sudo cp /vagrant/config/elasticsearch.yml /etc/elasticsearch/
 )
 
 # Installing Graylog-Server v0.20.0-preview.8
-graylog2_server="graylog2-server-0.20.0-preview.8"
-
 [[ ! -d ~/$graylog2_server ]] &&
 (
   echo " --- Installing Graylog-Server"
-  sudo tar xfz /vagrant/${graylog2_server}.tgz -C /opt/
+  sudo tar xfz /vagrant/src/${graylog2_server}.tgz -C /opt/
 
   sudo ln -s /opt/$graylog2_server/bin/graylog2ctl /etc/init.d/
-  sudo cp /vagrant/graylog2.conf /etc/graylog2.conf
+  sudo cp /vagrant/config/graylog2.conf /etc/graylog2.conf
 )
-
-graylog2_ui="graylog2-web-interface-0.20.0-preview.8"
 
 [[ ! -d ~/$graylog2_ui ]] &&
 (
-  echo " --- Installing Graylog-Server"
-  sudo tar xfz /vagrant/${graylog2_ui}.tgz -C /opt/
-  cp /vagrant/graylog2-web-interface.conf /opt/$graylog2_ui/conf/graylog2-web-interface.conf
+  echo " --- Installing Graylog-Web-Interface"
+  sudo tar xfz /vagrant/src/${graylog2_ui}.tgz -C /opt/
+  cp /vagrant/config/graylog2-web-interface.conf /opt/$graylog2_ui/conf/graylog2-web-interface.conf
   sudo ln -s /opt/$graylog2_ui/bin/graylog2-web-interface ~/
 )
 
 exit 0
+
